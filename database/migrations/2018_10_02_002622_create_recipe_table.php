d16e6a652d1e6d17027293dfa157be40a3c2ff88
Marissa Morgan <momorgan7@gmail.com>
2018-10-01 20:45:50 -0400

    add the starting migrations

---
+++ database/migrations/2018_10_02_002622_create_recipe_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipe', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('instructions');
+            $table->string('prep_time');
+            $table->string('cook_time');
+            $table->string('source');
+            $table->foreign('user_id')->references('id')->on('users')
+            $table->foreign('cusine_id')->references('id')->on('cusine')
+            $table->foreign('type_id')->references('id')->on('meal_type')
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipe');
+    }
+}
--- database/migrations/2018_10_02_003915_create_ingredients_table.php
+++ database/migrations/2018_10_02_003915_create_ingredients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateIngredientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ingredients', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolval('active');
-            $table->boolval('isAdmin');
-            $table->rememberToken();
+            $table->foreign('recipe_id')->references('id')->on('recipe');
+            $table->string('amount');
+            $table->string('unit');
+            $table->string('type');
+            $table->string('prep');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('ingredients');
     }
 }
